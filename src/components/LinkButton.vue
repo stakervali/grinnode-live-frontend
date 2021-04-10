6610639ee2089b5a3a502efc0de17354bc162b7f
stakervali <stakervali@yandex.com>
2021-04-10 14:18:56 +0300

    Created LinkButton component for easy sharing

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-inline">
+    <v-icon right @click="click" >link</v-icon>
+    <v-snackbar  top dark timeout="1500"  width="auto" tag="span" color="#fef102" v-model="snackbar"> Link copied! </v-snackbar>
+  </div>
+</template>
+
+<script>
+export default {
+name: "LinkButton",
+ data:()=>({
+   snackbar:false
+ }),
+ props:['copyLink'],
+ methods: {
+  click:function(){
+    console.log( this.copyLink +  " clicked");
+    navigator.clipboard.writeText(this.copyLink);
+    this.snackbar = true;
+  }
+ }
+}
+</script>
+
+<style scoped>
+.v-snack__wrapper {
+  max-width: none;
+}
+</style>
