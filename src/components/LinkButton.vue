628cf6f303f8fcf17588eb88d05b452daa837cf2
stakervali <stakervali@yandex.com>
2021-06-02 16:20:55 +0300

    Changed background color to gray for better readability

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-inline">
     <v-icon right @click="click" >link</v-icon>
-    <v-snackbar  top dark timeout="1500"  width="auto" tag="span" color="#fef102" v-model="snackbar"> Link copied! </v-snackbar>
+    <v-snackbar  top dark  text  timeout="1500"  width="auto" tag="span" color="#dddddd" v-model="snackbar"> <span  > Link copied! </span> </v-snackbar>
   </div>
 </template>
 
